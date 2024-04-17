5c9c4061e3bbf8dbdd2f7d0d0cbc2d5111eb96c6
mveluru <mveluru@gmail.com>
2024-04-17 15:28:52 -0500

    Refactored

--- src/com/brite/jee/practice1/ReverseInteger.java
+++ src/com/brite/jee/practice1/ReverseInteger.java
@@ -3,13 +3,13 @@ package com.brite.jee.practice1;
 public class ReverseInteger {
 
     public static void main(String[] args) {
-       Long x = Long.valueOf(Integer.MAX_VALUE)+1;
+       Long x = Long.valueOf(23232339);
 
         Long sum = Long.valueOf(0);
         while (x > 0) {
             Long reminder = x % 10;
             //System.out.print(reminder+" ");
-            sum = (sum * 10 + reminder) <= Integer.MAX_VALUE && ((sum * 10 + reminder) >= Integer.MIN_VALUE) ?  sum * 10 + reminder:0;
+            sum = (sum * 10 + reminder) <= Integer.MAX_VALUE && ((sum * 10 + reminder) >= 0) ?  sum * 10 + reminder:0;
             if (sum == 0) {
                 break;
             }
