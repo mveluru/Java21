c9e22dab3a5cefac71bf8a2fcf1b4734aa443917
mveluru <mveluru@gmail.com>
2024-04-21 19:48:12 -0500

    Tested

--- src/com/brite/jee/leet/Sum9.java
+++ src/com/brite/jee/leet/Sum9.java
@@ -3,16 +3,28 @@ package com.brite.jee.leet;
 public class Sum9 {
 
     public static void main(String[] args) {
-        int[] nums = {2, 1, 6, 4, 2, 1};
+        //int[] nums = {2, 1, 6, 4, 2, 1};
+        //int[] nums = {3, 1, 3, 3, 4};
+        //int[] nums = {1, 3, 3, 3, 1};
+        //int[] nums = {1, 3, 1, 1, 3, 3, 1};
+        //int[] nums = {3, 3, 1, 1, 1, 3, 1};
+        //int[] nums = {1, 1, 1, 1, 3, 3, 3};
+        int[] nums = {1, 1, 1, 1, 3, 3, 3, 1};
+
         int beginIndex = 0;
+        int nextIndex = beginIndex + 1;
         int endIndex = nums.length - 1;
-        while (beginIndex < endIndex) {
+        while (beginIndex < nextIndex && nextIndex <= endIndex) {
             boolean isvaluesFound = false;
-            for (int i = 0 ; i <=nums.length ; i++) {
-                if ((nums[beginIndex] + nums[endIndex] + nums[i] == 9) && (i!=beginIndex && i!=endIndex)) {
-                    System.out.println(nums[beginIndex] + " " + nums[endIndex] + " " + nums[i]);
-                    isvaluesFound = true;
-                    i=nums.length;
+            for (int i = 0; i < nums.length; i++) {
+                if ((i != beginIndex && i != nextIndex)) {
+                    if (nums[beginIndex] + nums[nextIndex] + nums[i] == 9) {
+                        System.out.println("index1: " + beginIndex + " index2: " + nextIndex + " index3: " + i);
+                        System.out.println(nums[beginIndex] + " " + nums[nextIndex] + " " + nums[i]);
+                        isvaluesFound = true;
+                        break;
+                        //i=nums.length-1;
+                    }
                 }
 
             }//for loop
@@ -21,9 +33,8 @@ public class Sum9 {
                 break;
             }
             beginIndex++;
-            endIndex--;
+            nextIndex++;
 
         }
-
     }
 }
