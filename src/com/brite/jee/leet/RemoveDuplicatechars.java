4a02bd6e7d49710e6cbb4068f76168503c9f17b2
mveluru <mveluru@gmail.com>
2024-05-05 20:46:13 -0500

    init

--- src/com/brite/jee/leet/RemoveDuplicatechars.java
+++ src/com/brite/jee/leet/RemoveDuplicatechars.java
@@ -3,30 +3,45 @@ package com.brite.jee.leet;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RemoveDuplicatechars {
 
+    public static String distinctchars_1(String input){
+
+        List<Character> abc = new LinkedList<>();
+        char[] inputchars = input.toCharArray();
+        for (char c: inputchars){
+            abc.add(c);
+        }
+        String string = abc.stream().distinct().map(String::valueOf).collect(Collectors.joining());
+        return string;
+    }
+
+    public static String distinctchars_2(String input){
+      String string =  Arrays.stream(input.split("")).distinct().map(String::valueOf).collect(Collectors.joining());
+      return string;
+    }
+
+    public static String distinctchars_3(String input){
+        IntStream intStream = input.chars().distinct();
+        int[] charintArrays = intStream.toArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i: charintArrays){
+            sb.append((char) i);
+        }
+        return sb.toString();
+    }
+
+
+
     public static void main(String[] args) {
         String input ="ddslkjfdskjdskkkdsla";
-      List<Character> abc = new LinkedList<>();
-       char[] inputchar = input.toCharArray();
-       for (char c: inputchar){
-           abc.add(c);
-       }
-       String string = abc.stream().distinct().map(String::valueOf).collect(Collectors.joining());
-        System.out.println(string);
 
-      String newString = Arrays.stream(input.split("")).distinct().map(String::valueOf).collect(Collectors.joining());
-        System.out.println(newString);
+        System.out.println(distinctchars_1(input));
 
-
-
-
-
-
-
-
-
+        System.out.println(distinctchars_2(input));
+        System.out.println(distinctchars_3(input));
 
     }
 }
