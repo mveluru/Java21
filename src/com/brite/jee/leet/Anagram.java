0ae9522f8409fc601c1a15025417a3a699548e7b
mveluru <mveluru@gmail.com>
2024-05-06 22:48:09 -0500

    Added another method

--- src/com/brite/jee/leet/Anagram.java
+++ src/com/brite/jee/leet/Anagram.java
@@ -33,15 +33,33 @@ public class Anagram {
         }
         return isAnagram;
     }
+    public static boolean isAnagram_string(String input1 ,String input2){
+
+        char[] input1chars = input1.toCharArray();
+        Arrays.sort(input1chars);
+        String input1String = new String(input1chars);
+
+        char[] input2chars = input2.toCharArray();
+        Arrays.sort(input2chars);
+        String input2String = new String((input2chars));
+
+        if (input2String.equals(input1String)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        //String a = "anagram";
-        //String b = "margana";
+        String a = "anagram";
+        String b = "margana";
 
-        String a = "anagramm";
-        String b = "marganaa";
-        boolean isAnagram = Anagram.isAnagram(a,b);
+        //String a = "anagramm";
+        //String b = "marganaa";
+        //boolean isAnagram = Anagram.isAnagram(a,b);
+        boolean isAnagram = Anagram.isAnagram_string(a,b);
         if (isAnagram){
             System.out.println("Anagrams");
         }else{
