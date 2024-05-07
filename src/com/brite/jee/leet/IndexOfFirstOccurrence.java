2e058c693dbaca0a91e846abed00ffc20937837d
mveluru <mveluru@gmail.com>
2024-05-07 18:21:13 -0500

    Added another method

--- src/com/brite/jee/leet/IndexOfFirstOccurrence.java
+++ src/com/brite/jee/leet/IndexOfFirstOccurrence.java
@@ -2,39 +2,50 @@ package com.brite.jee.leet;
 
 public class IndexOfFirstOccurrence {
 
-    private static void strStr(String haystack, String needle) {
-        if (haystack.indexOf(needle)!=-1) {
+    private static void firstoccurence1(String haystack, String needle) {
+        if (haystack.indexOf(needle) != -1) {
             int haystacklength = haystack.length();
             int needleLength = needle.length();
-            for(int i=0;i<haystacklength;i++){
-                String substring = haystack.substring(i,i+needleLength);
-                if (needle.equals(substring)){
-                //System.out.println(substring);
-                System.out.println("equal  "+"beginning Index :" +(i+1) + " ending index  "+((i+needleLength)));
-                break;
+            for (int i = 0; i < haystacklength; i++) {
+                if ((i + needleLength) < haystacklength) {
+                    String substring = haystack.substring(i, i + needleLength);
+                    if (needle.equals(substring)) {
+                        //System.out.println(substring);
+                        System.out.println( "start_index : " + (i) + " end_index  " + ((i + needleLength-1)));
+                        break;
+                    }
+
                 }
             }
 
-        }else{
+        } else {
             System.out.println(-1);
             //System.out.println(0);
         }
     }
 
-
+    private static void firstoccurence2(String haystack, String needle) {
+        if (haystack.toLowerCase().contains(needle.toLowerCase())) {
+            int start_index = haystack.indexOf(needle);
+            System.out.println("start_index : " +start_index + " end_index: " + (start_index + needle.length() - 1));
+        } else {
+            System.out.println(-1);
+        }
+    }
 
     public static void main(String[] args) {
-        //String haystack = "leetcode";
-        //String needle = "leetco";
+        String haystack = "leetcode";
+        String needle = "leetco";
         //String haystack = "sadbutsad";
         //String needle = "sad";
         //String haystack = "greekforgreek";
         //String needle = "for";
-        String haystack ="kavihellohello";
-        String needle = "hello";
+       // String haystack = "kavihellohello";
+        //String needle = "hello";
         //String haystack ="dubaikamir";
         //String needle = "mir";
 
-        IndexOfFirstOccurrence.strStr(haystack, needle);
+        IndexOfFirstOccurrence.firstoccurence1(haystack, needle);
+        IndexOfFirstOccurrence.firstoccurence2(haystack, needle);
     }
 }
