ed9faac18bc040036320cb96cd63935334c62d3a
mveluru <mveluru@gmail.com>
2026-03-15 13:21:27 -0500

    New changes

--- src/com/java21/algorithms/LNode.java
+++ src/com/java21/algorithms/LNode.java
@@ -1,4 +1,29 @@
 package com.java21.algorithms;
 
 public class LNode {
+    public int data;
+
+    public LNode nextRef;
+
+    public LNode(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public LNode getNextRef() {
+        return nextRef;
+    }
+
+    public void setNextRef(LNode nextRef) {
+        this.nextRef = nextRef;
+    }
+
+
 }
