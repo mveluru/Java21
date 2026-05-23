e781e7a8731694b1621c83e81dbd23eead3b9d6f
mveluru <mveluru@gmail.com>
2026-05-23 08:45:03 -0500

    New changes

--- src/com/interview2026/AnagaramExample.java
+++ src/com/interview2026/AnagaramExample.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 
 public class AnagaramExample {
     public static void main(String[] args) {
-        String s1 ="Silent".toLowerCase();
-        String s2 ="listen".toLowerCase();
-       char[] s1char = s1.toCharArray();
-       char[] s2char = s2.toCharArray();
-       Arrays.sort(s1char);
-       Arrays.sort(s2char);
-       if(s1.equals(s2)){
-           System.out.println("Yes");
-       }else{
-           System.out.println("No");
-       }
+        String s1 = "Silent".toLowerCase();
+        String s2 = "listen".toLowerCase();
+        char[] s1char = s1.toCharArray();
+        char[] s2char = s2.toCharArray();
+        Arrays.sort(s1char);
+        Arrays.sort(s2char);
+        if (Arrays.equals(s1char,s2char)) {
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
     }
 }
